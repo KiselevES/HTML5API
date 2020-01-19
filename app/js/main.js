5ee10187a66b8c5dd661855d91a80fe3db525ef4
Egor Kiselev <kiselev_es@icloud.com>
2020-01-19 17:46:53 +0300

    Добавлено время на видео плеер

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,39 @@ $(document).ready(function () {
     $('.play').on('click', function (e) {
         e.preventDefault();
         var $this = $(this);
-        if(video.paused) {
+        if (video.paused) {
             video.play();
-            this.text = '||';
+            $this.text('||');
         } else {
             video.pause();
-            this.text = '>';
+            $this.text('>');
         }
     });
+
+    $(video).on('timeupdate', function () {
+        var currentTime = video.currentTime,
+            duration = video.duration,
+            elapsedTime = duration - currentTime,
+            minutesLeft = Math.floor(elapsedTime / 60),
+            secondsLeft = Math.floor(elapsedTime - (minutesLeft * 60));
+
+        if (secondsLeft.toString().length === 1) {
+            secondsLeft ="0" + secondsLeft;
+        }
+        var timeString = minutesLeft + ':' + secondsLeft;
+        $('.time').text(timeString);
+    });
+
+    $(video).on('canplay', function () {
+        var time = video.duration,
+            minutes = Math.floor(time / 60),
+            seconds = Math.floor(time - (minutes * 60));
+
+        if( seconds.toString().length ===1){
+            seconds = '0' + seconds;
+        }
+
+        timeString = minutes + ":" + seconds;
+        $('.time').text(timeString);
+    })
 });
